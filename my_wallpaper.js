f65b81ef28ae90958ff9cd11023d02666277e509
syong1237 <syong1237@outlook.com>
2024-08-01 12:06:47 +1200

    settings

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,20 +1,32 @@
 //your parameter variables go here!
-let pith_size = 10;
 
-let pith_white = '#ffffff';
+let pith_size = 10; //changes the size of the pith core 
+
+let pith_white = '#ffffff'; //Changes the colour of the pith lines,changes automatically via if statment
 let pith_black = '#000000';
 
-let seed_size = 5 ;
+let seed_size = 5 ; //Changes the size of seeds
 
-let lime_skin = '#32CD32';
+let lime_skin = '#32CD32'; // Lime Colour scheme
 let lime_flesh = '#90EE90';
 
-let lemon_skin = '#fff44f';
+let lemon_skin = '#fff44f'; // Lemon Colour Scheme
 let lemon_flesh = '#fffed3';
 
-let orange_skin = '#F28C28';
+let orange_skin = '#F28C28'; //Orange Colour Scheme
 let orange_flesh = '#FFAC1C';
 
+// line 61: citrus(orange_skin,orange_flesh) //Top Left
+
+// line 67: citrus(lime_skin,lime_flesh)  //Top Right
+
+// line 72: citrus(lime_skin,lime_flesh) //Bottom Left
+
+// line 78: citrus(orange_skin,orange_flesh) //Bottom Right
+
+// line 84: citrus(lemon_skin,lemon_flesh) //Middle Left
+
+// line 90: citrus(lemon_skin,lemon_flesh) //Middle Right
 
 //For Copy/Paste
 
@@ -25,8 +37,8 @@ let orange_flesh = '#FFAC1C';
 //  lemon_skin,lemon_flesh
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -42,7 +54,6 @@ function wallpaper_background() {
 }
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
-  citrus(lime_skin, lime_flesh) //bottom left
   
   push()//Top left
   rotate(93)
@@ -53,8 +64,13 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   push()//Top right
   rotate(181)
   translate(-202,-196)
-  citrus(lime_skin,lime_flesh)
+  citrus(lime_skin, lime_flesh)
   pop()
+
+
+  //Bottom left
+  citrus(lime_skin, lime_flesh)
+
 
   push()//Bottom right
   rotate(274)
@@ -108,7 +124,7 @@ function citrus(skin, flesh) {
   fill(255,255,255);
   arc(54,146.1,pith_size,pith_size,45,220,CHORD);
   
-
+//Seed
  ellipse(38,140,seed_size/2,seed_size)
  ellipse(61,161,seed_size,seed_size/2)
 
